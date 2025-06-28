aadd86c16d43d7a3c43d3ed5725ae49e5eed3d48
genir <45612481+Halke1986@users.noreply.github.com>
2025-06-29 00:18:15 +0200

    scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,3 @@
+rm -r                              "$SS_PATH/starsector-core/fs.common_obf.jar"
+
+cp ./jars/out/fs.common_obf.jar    "$SS_PATH/starsector-core/fs.common_obf.jar"
---
+++ scripts/merge_renderer.sh
@@ -0,0 +1,3 @@
+rm -r /jars/out/fs.common_obf.jar
+
+zipmerge -s ./jars/out/fs.common_obf.jar ./jars/starfarer/fs.common_obf.jar ./jars/build/renderer.jar
