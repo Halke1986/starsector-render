523fe41aaabdf05aa7f012336ff812b0bb1f69a1
genir <45612481+Halke1986@users.noreply.github.com>
2025-06-28 23:49:34 +0200

    disassemble

--- disassemble.sh
+++
@@ -1,6 +0,0 @@
-rm -rf ./disassembly
-mkdir -p ./disassembly
-
-python3 ../../../Krakatau/disassemble.py -out ./disassembly/starfarer ./starfarer/starfarer_obf.jar
-python3 ../../../Krakatau/disassemble.py -out ./disassembly/api ./starfarer/starfarer.api.jar
-python3 ../../../Krakatau/disassemble.py -out ./disassembly/common ./starfarer/fs.common_obf.jar
---
+++ disassemble_starfarer.sh
@@ -0,0 +1,6 @@
+rm -rf ./disassembly/starfarer/
+mkdir -p ./disassembly/starfarer/
+
+python3 ../../../Krakatau/disassemble.py -out ./disassembly/starfarer/starfarer ./jars/starfarer/starfarer_obf.jar
+python3 ../../../Krakatau/disassemble.py -out ./disassembly/starfarer/api       ./jars/starfarer/starfarer.api.jar
+python3 ../../../Krakatau/disassemble.py -out ./disassembly/starfarer/common    ./jars/starfarer/fs.common_obf.jar
