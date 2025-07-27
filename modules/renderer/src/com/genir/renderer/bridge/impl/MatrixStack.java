02302292cd1f3e21d2deda57b5b58997325b3d6b
genir <45612481+Halke1986@users.noreply.github.com>
2025-07-27 16:05:47 +0200

    glRotatef fast path

--- modules/renderer/src/com/genir/renderer/bridge/impl/MatrixStack.java
+++ modules/renderer/src/com/genir/renderer/bridge/impl/MatrixStack.java
@@ -64,55 +64,78 @@ public class MatrixStack {
         // xzk-ys yzk+xs zzk+1c 0
         // 0      0      0      1
 
-        Matrix4f m = stack[matrixIdx];
-
-        float l = (float) sqrt(x * x + y * y + z * z);
-        float xn = x / l;
-        float yn = y / l;
-        float zn = z / l;
-
         float a = angle * (float) (Math.PI / 180);
         float c = (float) cos(a);
         float s = (float) Math.sin(a);
-        float k = 1 - c;
 
-        float r00 = xn * xn * k + c;
-        float r01 = xn * yn * k - zn * s;
-        float r02 = xn * zn * k + yn * s;
+        Matrix4f m = stack[matrixIdx];
 
-        float r10 = xn * yn * k + zn * s;
-        float r11 = yn * yn * k + c;
-        float r12 = yn * zn * k - xn * s;
+        if (x == 0f && y == 0f && z == 1f) {
+            // 2D fast path.
+            float m00 = c * m.m00 + s * m.m01;
+            float m01 = -s * m.m00 + c * m.m01;
+            float m10 = c * m.m10 + s * m.m11;
+            float m11 = -s * m.m10 + c * m.m11;
+            float m20 = c * m.m20 + s * m.m21;
+            float m21 = -s * m.m20 + c * m.m21;
+            float m30 = c * m.m30 + s * m.m31;
+            float m31 = -s * m.m30 + c * m.m31;
 
-        float r20 = xn * zn * k - yn * s;
-        float r21 = yn * zn * k + xn * s;
-        float r22 = zn * zn * k + c;
+            m.m00 = m00;
+            m.m01 = m01;
+            m.m10 = m10;
+            m.m11 = m11;
+            m.m20 = m20;
+            m.m21 = m21;
+            m.m30 = m30;
+            m.m31 = m31;
+        } else {
+            // 3D path.
+            float l = (float) sqrt(x * x + y * y + z * z);
+            float xn = x / l;
+            float yn = y / l;
+            float zn = z / l;
 
-        float m00 = r00 * m.m00 + r10 * m.m01 + r20 * m.m02;
-        float m01 = r01 * m.m00 + r11 * m.m01 + r21 * m.m02;
-        float m02 = r02 * m.m00 + r12 * m.m01 + r22 * m.m02;
-        float m10 = r00 * m.m10 + r10 * m.m11 + r20 * m.m12;
-        float m11 = r01 * m.m10 + r11 * m.m11 + r21 * m.m12;
-        float m12 = r02 * m.m10 + r12 * m.m11 + r22 * m.m12;
-        float m20 = r00 * m.m20 + r10 * m.m21 + r20 * m.m22;
-        float m21 = r01 * m.m20 + r11 * m.m21 + r21 * m.m22;
-        float m22 = r02 * m.m20 + r12 * m.m21 + r22 * m.m22;
-        float m30 = r00 * m.m30 + r10 * m.m31 + r20 * m.m32;
-        float m31 = r01 * m.m30 + r11 * m.m31 + r21 * m.m32;
-        float m32 = r02 * m.m30 + r12 * m.m31 + r22 * m.m32;
+            float k = 1 - c;
 
-        m.m00 = m00;
-        m.m01 = m01;
-        m.m02 = m02;
-        m.m10 = m10;
-        m.m11 = m11;
-        m.m12 = m12;
-        m.m20 = m20;
-        m.m21 = m21;
-        m.m22 = m22;
-        m.m30 = m30;
-        m.m31 = m31;
-        m.m32 = m32;
+            float r00 = xn * xn * k + c;
+            float r01 = xn * yn * k - zn * s;
+            float r02 = xn * zn * k + yn * s;
+
+            float r10 = xn * yn * k + zn * s;
+            float r11 = yn * yn * k + c;
+            float r12 = yn * zn * k - xn * s;
+
+            float r20 = xn * zn * k - yn * s;
+            float r21 = yn * zn * k + xn * s;
+            float r22 = zn * zn * k + c;
+
+            float m00 = r00 * m.m00 + r10 * m.m01 + r20 * m.m02;
+            float m01 = r01 * m.m00 + r11 * m.m01 + r21 * m.m02;
+            float m02 = r02 * m.m00 + r12 * m.m01 + r22 * m.m02;
+            float m10 = r00 * m.m10 + r10 * m.m11 + r20 * m.m12;
+            float m11 = r01 * m.m10 + r11 * m.m11 + r21 * m.m12;
+            float m12 = r02 * m.m10 + r12 * m.m11 + r22 * m.m12;
+            float m20 = r00 * m.m20 + r10 * m.m21 + r20 * m.m22;
+            float m21 = r01 * m.m20 + r11 * m.m21 + r21 * m.m22;
+            float m22 = r02 * m.m20 + r12 * m.m21 + r22 * m.m22;
+            float m30 = r00 * m.m30 + r10 * m.m31 + r20 * m.m32;
+            float m31 = r01 * m.m30 + r11 * m.m31 + r21 * m.m32;
+            float m32 = r02 * m.m30 + r12 * m.m31 + r22 * m.m32;
+
+            m.m00 = m00;
+            m.m01 = m01;
+            m.m02 = m02;
+            m.m10 = m10;
+            m.m11 = m11;
+            m.m12 = m12;
+            m.m20 = m20;
+            m.m21 = m21;
+            m.m22 = m22;
+            m.m30 = m30;
+            m.m31 = m31;
+            m.m32 = m32;
+        }
     }
 
     public void glScalef(float x, float y, float z) {
