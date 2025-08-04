f94cef08deb8d034a4e140cc8f396334023d3ed8
genir <45612481+Halke1986@users.noreply.github.com>
2025-08-04 18:52:43 +0200

    remove overrides

--- modules/methods/src/com/fs/starfarer/combat/entities/CustomCombatEntity.java
+++ modules/methods/src/com/fs/starfarer/combat/entities/CustomCombatEntity.java
@@ -1,25 +1,11 @@
 package com.fs.starfarer.combat.entities;
 
-import com.fs.starfarer.api.combat.CombatEngineLayers;
 import com.fs.starfarer.api.combat.CombatLayeredRenderingPlugin;
-import com.fs.starfarer.combat.CombatViewport;
-import com.genir.renderer.overrides.RenderOverride;
 
-public class CustomCombatEntity extends CombatEntityAPIImpl {
+public class CustomCombatEntity {
     private CombatLayeredRenderingPlugin plugin;
 
     public CombatLayeredRenderingPlugin getPlugin() {
         return null;
-    }
-
-    // REPLACED METHOD
-    public void render(CombatEngineLayers var1, CombatViewport viewport) {
-        if (!RenderOverride.shouldRender(this.plugin)) {
-            return;
-        }
-
-        if (viewport.isNearViewport(this.getLocation(), this.plugin.getRenderRadius())) {
-            this.plugin.render(var1, viewport);
-        }
     }
 }
--- modules/methods/src/com/fs/starfarer/combat/entities/terrain/Planet.java
+++
@@ -1,4 +0,0 @@
-package com.fs.starfarer.combat.entities.terrain;
-
-public class Planet {
-}
