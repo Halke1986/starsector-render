01a6c9935d227d8cdab7c0fc18dc499a035b3693
genir <45612481+Halke1986@users.noreply.github.com>
2025-08-04 17:30:41 +0200

    remove CombatState

--- modules/renderer/src/com/genir/renderer/overrides/CombatState.java
+++
@@ -1,9 +0,0 @@
-package com.genir.renderer.overrides;
-
-import java.io.IOException;
-
-public class CombatState {
-    public static String traverse(com.fs.starfarer.combat.CombatState state) throws IOException {
-        return state.traverseImpl();
-    }
-}
