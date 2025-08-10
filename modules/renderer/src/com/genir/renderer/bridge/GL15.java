0cc1b330381b07dc1fc1656d79220f4856f393d0
genir <45612481+Halke1986@users.noreply.github.com>
2025-08-11 01:36:28 +0200

    decorate gl15

--- modules/renderer/src/com/genir/renderer/bridge/GL15.java
+++ modules/renderer/src/com/genir/renderer/bridge/GL15.java
@@ -3,55 +3,54 @@ package com.genir.renderer.bridge;
 import com.genir.renderer.bridge.impl.BufferUtil;
 
 import java.nio.FloatBuffer;
+import java.util.concurrent.Callable;
 
 import static com.genir.renderer.bridge.impl.Bridge.exec;
 
 public class GL15 {
     public static void glBindBuffer(int target, int buffer) {
-//        record glEnd() implements Runnable {
-//            @Override
-//            public void run() {
-////                if (listManager.isRecording(this))
-////                    return;
-//
-//                vertexInterceptor.glEnd();
-//            }
-//        }
-//        exec.execute(new glEnd());
+        record glBindBuffer(int target, int buffer) implements Runnable {
+            @Override
+            public void run() {
+                //        clientAttribTracker.glVertexPointer(0, 0, null);
+                //        clientAttribTracker.glColorPointer(0, false, 0, null);
+                //        clientAttribTracker.glTexCoordPointer(0, 0, null);
 
-//        clientAttribTracker.glVertexPointer(0, 0, null);
-//        clientAttribTracker.glColorPointer(0, false, 0, null);
-//        clientAttribTracker.glTexCoordPointer(0, 0, null);
-
-//        vertexInterceptor.arraysTouched();
-
-        exec.execute(() -> org.lwjgl.opengl.GL15.glBindBuffer(target, buffer));
+                //        vertexInterceptor.arraysTouched();
+                org.lwjgl.opengl.GL15.glBindBuffer(target, buffer);
+            }
+        }
+        exec.execute(new glBindBuffer(target, buffer));
     }
 
     public static void glDeleteBuffers(int buffer) {
-        exec.execute(() -> org.lwjgl.opengl.GL15.glDeleteBuffers(buffer));
+        record glDeleteBuffers(int buffer) implements Runnable {
+            @Override
+            public void run() {
+                org.lwjgl.opengl.GL15.glDeleteBuffers(buffer);
+            }
+        }
+        exec.execute(new glDeleteBuffers(buffer));
     }
 
-    /**
-     * Blocking.
-     */
     public static int glGenBuffers() {
-        return exec.get(() -> org.lwjgl.opengl.GL15.glGenBuffers());
+        record glGenBuffers() implements Callable<Integer> {
+            @Override
+            public Integer call() {
+                return org.lwjgl.opengl.GL15.glGenBuffers();
+            }
+        }
+        return exec.get(new glGenBuffers());
     }
 
     public static void glBufferData(int target, FloatBuffer data, int usage) {
-//        record glEnd() implements Runnable {
-//            @Override
-//            public void run() {
-//                if (listManager.isRecording(this))
-//                    return;
-//
-//                vertexInterceptor.glEnd();
-//            }
-//        }
-//        exec.execute(new glEnd());
-
+        record glBufferData(int target, FloatBuffer data, int usage) implements Runnable {
+            @Override
+            public void run() {
+                org.lwjgl.opengl.GL15.glBufferData(target, data, usage);
+            }
+        }
         final FloatBuffer snapshot = BufferUtil.snapshot(data);
-        exec.execute(() -> org.lwjgl.opengl.GL15.glBufferData(target, snapshot, usage));
+        exec.execute(new glBufferData(target, snapshot, usage));
     }
 }
