d6ee7c5f6d4ff22efba7a8f577ce5a15b8be6c82
genir <45612481+Halke1986@users.noreply.github.com>
2025-08-23 15:40:48 +0200

    script loader findClass method

--- modules/renderer/src/com/genir/renderer/overrides/ScriptLoader.java
+++ modules/renderer/src/com/genir/renderer/overrides/ScriptLoader.java
@@ -1,8 +1,13 @@
 package com.genir.renderer.overrides;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.security.CodeSigner;
+import java.security.CodeSource;
+import java.security.ProtectionDomain;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,51 +26,76 @@ public class ScriptLoader extends URLClassLoader {
             ClassConstantTransformer.newTransform("org/lwjgl/opengl/GL33", "com/genir/renderer/bridge/GL33"),
             ClassConstantTransformer.newTransform("org/lwjgl/opengl/GL43", "com/genir/renderer/bridge/GL43"),
             ClassConstantTransformer.newTransform("org/lwjgl/opengl/Display", "com/genir/renderer/bridge/Display"),
-            ClassConstantTransformer.newTransform("org/lwjgl/opengl/GLContext", "com/genir/renderer/bridge/GLContext")
+            ClassConstantTransformer.newTransform("org/lwjgl/opengl/GLContext", "com/genir/renderer/bridge/GLContext"),
+            ClassConstantTransformer.newTransform("java/net/URLClassLoader", "com/genir/renderer/overrides/ScriptLoader")
     ));
 
     public ScriptLoader(URL[] urls, ClassLoader parent) {
         super(urls, parent);
     }
 
+    public ScriptLoader(URL[] urls) {
+        super(urls);
+    }
+
     @Override
     public Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-        // Do not try to transform Starsector core and libraries.
-        try {
-            return ClassLoader.getSystemClassLoader().loadClass(name);
-        } catch (ClassNotFoundException ignored) {
-        }
-
-        Class<?> cached = cache.get(name);
+        Class<?> cached = findLoadedClass(name);
         if (cached != null) {
             return cached;
         }
 
-        InputStream classStream = super.getResourceAsStream(name.replace('.', '/') + ".class");
-        if (classStream == null) {
-            return super.loadClass(name, resolve);
-        }
-
+        // Do not try to transform Starsector core and libraries.
         try {
-            byte[] originalBytes = classStream.readAllBytes();
-            byte[] transformedBytes = transformer.apply(originalBytes);
-
-            Class<?> c;
-
-            if (Arrays.equals(originalBytes, transformedBytes)) {
-                // Use the original class if no transformation was required.
-                // This helps to prevent errors with mods that use custom
-                // class loaders.
-                c = super.loadClass(name, resolve);
-            } else {
-                // Define the transformed class.
-                c = super.defineClass(name, transformedBytes, 0, transformedBytes.length);
-            }
-
-            cache.put(name, c);
-            return c;
-        } catch (Throwable e) {
+//            return ClassLoader.getSystemClassLoader().loadClass(name);
             return super.loadClass(name, resolve);
+        } catch (ClassNotFoundException ignored) {
         }
+
+        return findClass(name);
+    }
+
+    @Override
+    public Class<?> findClass(String name) throws ClassNotFoundException {
+        String binaryName = name.replace('.', '/') + ".class";
+
+        if (name.contains("RefitTabListenerAndScript")) {
+//            logStack();
+        }
+
+        // Get class resource stream.
+        InputStream classStream = super.getResourceAsStream(binaryName);
+        if (classStream == null) {
+            throw new ClassNotFoundException(name);
+        }
+
+        // Read class bytes.
+        byte[] originalBytes;
+        try {
+            originalBytes = classStream.readAllBytes();
+        } catch (IOException e) {
+            throw new ClassNotFoundException(name, e);
+        }
+
+        // Read class code source.
+        URL url = super.findResource(binaryName);
+        if (url != null) {
+            String urlStr = url.toString();
+            int i = urlStr.lastIndexOf(binaryName);
+            if (i > 0) {
+                try {
+                    url = new URL(urlStr.substring(0, i));
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        CodeSource source = new CodeSource(url, (CodeSigner[]) null);
+        ProtectionDomain pd = new ProtectionDomain(source, null, this, null);
+
+        // Define transformed class.
+        byte[] transformedBytes = transformer.apply(originalBytes);
+        return super.defineClass(name, transformedBytes, 0, transformedBytes.length, pd);
     }
 }
