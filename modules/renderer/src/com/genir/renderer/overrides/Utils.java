cf7e59f581116c18fba422c316b30b257bd9d0bb
genir <45612481+Halke1986@users.noreply.github.com>
2025-10-10 21:30:16 +0200

    optimize File.exists

--- modules/renderer/src/com/genir/renderer/overrides/Utils.java
+++ modules/renderer/src/com/genir/renderer/overrides/Utils.java
@@ -5,38 +5,36 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Utils {
-    private static final Set<String> directories = new HashSet<>();
-    private static final Set<Path> files = new HashSet<>();
+    private static final Map<String, Set<Path>> directories = new HashMap<>();
 
     public static boolean exists(File file) {
+        if (file == null) {
+            return false;
+        }
+
         try {
-            if (file == null) {
-                return false;
-            }
-
             Path path = Paths.get(file.getPath());
-            if (files.contains(path)) {
-                return true;
-            }
-
             Path directory = path.getParent();
-            if (directory == null) {
-                return Files.exists(path);
-            }
 
-            if (directories.contains(directory.toString())) {
-                return false;
-            }
+            Set<Path> files = directories.get(directory.toString());
+            if (files == null) {
+                files = new HashSet<>();
 
-            directories.add(directory.toString());
-            try (DirectoryStream<Path> stream = Files.newDirectoryStream(directory)) {
-                for (Path entry : stream) {
-                    files.add(entry);
+                try (DirectoryStream<Path> stream = Files.newDirectoryStream(directory)) {
+                    for (Path entry : stream) {
+                        files.add(entry);
+                    }
+                } catch (Throwable ignored) {
+                    // Not a valid directory.
                 }
+
+                directories.put(directory.toString(), files);
             }
 
             return files.contains(path);
