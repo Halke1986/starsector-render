8b05c216569ee18e0d6ea3e90eba21ee9a575c32
genir <45612481+Halke1986@users.noreply.github.com>
2025-11-18 01:44:34 +0100

    custom FileRepository

--- modules/renderer/src/com/genir/renderer/overrides/FileRepository.java
+++ modules/renderer/src/com/genir/renderer/overrides/FileRepository.java
@@ -1,26 +1,77 @@
 package com.genir.renderer.overrides;
 
+import com.genir.renderer.async.ExecutorFactory;
+import org.apache.log4j.Logger;
+
 import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 public class FileRepository {
+    private static final Map<String, Future<BufferedImage>> images = new HashMap<>();
+    private static final Map<String, Future<byte[]>> sounds = new HashMap<>();
+
+    private static final ExecutorService exec = ExecutorFactory.newSingleThreadExecutor("FR-File-Repository");
+    private static final Logger logger = Logger.getLogger(FileRepository.class);
+
     public static void queueImage(String path) {
+        if (path != null && !images.containsKey(path)) {
+            images.put(path, exec.submit(() -> loadImage(path)));
+        }
     }
 
     public static void queueSound(String path) {
-    }
-
-    public static byte[] getSound(String path) {
-        try {
-            return com.fs.graphics.FileRepository.loadSound(path);
-        } catch (Exception ignored) {
-            return null;
+        if (path != null && !sounds.containsKey(path)) {
+            sounds.put(path, exec.submit(() -> loadSound(path)));
         }
     }
 
     public static BufferedImage getImage(String path) {
+        Future<BufferedImage> future = images.get(path);
+        if (future == null) {
+            return loadImage(path);
+        }
+
         try {
-            return com.fs.graphics.FileRepository.loadImage(path);
+            return future.get();
         } catch (Exception ignored) {
+            return loadImage(path);
+        }
+    }
+
+    public static byte[] getSound(String path) {
+        Future<byte[]> future = sounds.get(path);
+        if (future == null) {
+            return loadSound(path);
+        }
+
+        try {
+            return future.get();
+        } catch (Exception ignored) {
+            return loadSound(path);
+        }
+    }
+
+    private static byte[] loadSound(String path) {
+        try {
+            byte[] bytes = com.fs.graphics.FileRepository.loadSound(path);
+//            logger.info("Loaded file [" + path + "]");
+            return bytes;
+        } catch (Exception e) {
+            logger.error("Error while loading file [" + path + "]: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private static BufferedImage loadImage(String path) {
+        try {
+            BufferedImage bytes = com.fs.graphics.FileRepository.loadImage(path);
+//            logger.info("Loaded file [" + path + "]");
+            return bytes;
+        } catch (Exception e) {
+            logger.error("Error while loading file [" + path + "]: " + e.getMessage());
             return null;
         }
     }
@@ -29,5 +80,6 @@ public class FileRepository {
     }
 
     public static void joinThread() {
+        exec.shutdown();
     }
 }
--- modules/renderer/src/com/genir/renderer/overrides/ResourceLoadingState.java
+++ modules/renderer/src/com/genir/renderer/overrides/ResourceLoadingState.java
@@ -2,6 +2,14 @@ package com.genir.renderer.overrides;
 
 public class ResourceLoadingState {
     public static void loadResource(String type, String path) {
-        int x = 1;
+        switch (type) {
+            case "TEXTURE":
+            case "TEXTURE_OPTIONAL":
+            case "TEXTURE_ALPHA_ADDER":
+                FileRepository.queueImage(path);
+                break;
+            case "SOUND":
+                FileRepository.queueSound(path);
+        }
     }
 }
