1687cc0b3da631f03919ad9d9489850d9fbc32c3
genir <45612481+Halke1986@users.noreply.github.com>
2025-11-29 23:57:35 +0100

    cleanup methods

--- modules/callees/src/com/fs/starfarer/combat/entities/CustomCombatEntity.java
+++ modules/callees/src/com/fs/starfarer/combat/entities/CustomCombatEntity.java
@@ -3,8 +3,6 @@ package com.fs.starfarer.combat.entities;
 import com.fs.starfarer.api.combat.CombatLayeredRenderingPlugin;
 
 public class CustomCombatEntity {
-    private CombatLayeredRenderingPlugin plugin;
-
     public CombatLayeredRenderingPlugin getPlugin() {
         return null;
     }
--- modules/methods/src/com/fs/starfarer/combat/CombatViewport.java
+++
@@ -1,4 +0,0 @@
-package com.fs.starfarer.combat;
-
-public class CombatViewport extends CombatViewportAPIImlp {
-}
