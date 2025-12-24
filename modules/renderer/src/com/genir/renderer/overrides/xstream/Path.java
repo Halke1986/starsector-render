fde7d4155b2af2ebe21a50f98042c2c1945baf62
genir <45612481+Halke1986@users.noreply.github.com>
2025-12-24 23:47:08 +0100

    Path optimization I

--- modules/renderer/src/com/genir/renderer/overrides/xstream/Path.java
+++ modules/renderer/src/com/genir/renderer/overrides/xstream/Path.java
@@ -3,11 +3,16 @@ package com.genir.renderer.overrides.xstream;
 import com.thoughtworks.xstream.core.util.FastStack;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Path {
+    private static Map<Integer, String> chunkValueMap = new HashMap<>();
+    private static Map<String, Integer> chunkIDMap = new HashMap<>();
 
-    private final String[] chunks;
+    private final int[] chunkIDs;
+
     private transient String pathAsString;
     private transient String pathExplicit;
     private static final Path DOT = new Path(new String[]{"."});
@@ -26,7 +31,8 @@ public class Path {
         result.add(normalize(pathAsString, currentIndex, pathAsString.length()));
         String[] arr = new String[result.size()];
         result.toArray(arr);
-        chunks = arr;
+
+        this.chunkIDs = packChunks(arr);
     }
 
     private String normalize(String s, int start, int end) {
@@ -39,19 +45,18 @@ public class Path {
         } else {
             return s.substring(start, end);
         }
-
     }
 
     public Path(String[] chunks) {
-        this.chunks = chunks;
+        this.chunkIDs = packChunks(chunks);
     }
 
     public String toString() {
         if (pathAsString == null) {
             StringBuffer buffer = new StringBuffer();
-            for (int i = 0; i < chunks.length; i++) {
+            for (int i = 0; i < chunkIDs.length; i++) {
                 if (i > 0) buffer.append('/');
-                buffer.append(chunks[i]);
+                buffer.append(getChunk(i));
             }
             pathAsString = buffer.toString();
         }
@@ -61,9 +66,9 @@ public class Path {
     public String explicit() {
         if (pathExplicit == null) {
             StringBuffer buffer = new StringBuffer();
-            for (int i = 0; i < chunks.length; i++) {
+            for (int i = 0; i < chunkIDs.length; i++) {
                 if (i > 0) buffer.append('/');
-                String chunk = chunks[i];
+                String chunk = getChunk(i);
                 buffer.append(chunk);
                 int length = chunk.length();
                 if (length > 0) {
@@ -83,9 +88,9 @@ public class Path {
         if (!(o instanceof Path)) return false;
 
         final Path other = (Path) o;
-        if (chunks.length != other.chunks.length) return false;
-        for (int i = 0; i < chunks.length; i++) {
-            if (!chunks[i].equals(other.chunks[i])) return false;
+        if (chunkIDs.length != other.chunkIDs.length) return false;
+        for (int i = 0; i < chunkIDs.length; i++) {
+            if (chunkIDs[i] != other.chunkIDs[i]) return false;
         }
 
         return true;
@@ -93,22 +98,22 @@ public class Path {
 
     public int hashCode() {
         int result = 543645643;
-        for (int i = 0; i < chunks.length; i++) {
-            result = 29 * result + chunks[i].hashCode();
+        for (int i = 0; i < chunkIDs.length; i++) {
+            result = 29 * result + new Integer(chunkIDs[i]).hashCode();
         }
         return result;
     }
 
     public Path relativeTo(Path that) {
-        int depthOfPathDivergence = depthOfPathDivergence(chunks, that.chunks);
-        String[] result = new String[chunks.length + that.chunks.length - 2 * depthOfPathDivergence];
+        int depthOfPathDivergence = depthOfPathDivergence(unpackChunks(chunkIDs), unpackChunks(that.chunkIDs));
+        String[] result = new String[chunkIDs.length + that.chunkIDs.length - 2 * depthOfPathDivergence];
         int count = 0;
 
-        for (int i = depthOfPathDivergence; i < chunks.length; i++) {
+        for (int i = depthOfPathDivergence; i < chunkIDs.length; i++) {
             result[count++] = "..";
         }
-        for (int j = depthOfPathDivergence; j < that.chunks.length; j++) {
-            result[count++] = that.chunks[j];
+        for (int j = depthOfPathDivergence; j < that.chunkIDs.length; j++) {
+            result[count++] = that.getChunk(j);
         }
 
         if (count == 0) {
@@ -131,12 +136,12 @@ public class Path {
     public Path apply(Path relativePath) {
         FastStack absoluteStack = new FastStack(16);
 
-        for (int i = 0; i < chunks.length; i++) {
-            absoluteStack.push(chunks[i]);
+        for (int i = 0; i < chunkIDs.length; i++) {
+            absoluteStack.push(getChunk(i));
         }
 
-        for (int i = 0; i < relativePath.chunks.length; i++) {
-            String relativeChunk = relativePath.chunks[i];
+        for (int i = 0; i < relativePath.chunkIDs.length; i++) {
+            String relativeChunk = relativePath.getChunk(i);
             if (relativeChunk.equals("..")) {
                 absoluteStack.pop();
             } else if (!relativeChunk.equals(".")) {
@@ -153,14 +158,43 @@ public class Path {
     }
 
     public boolean isAncestor(Path child) {
-        if (child == null || child.chunks.length < chunks.length) {
+        if (child == null || child.chunkIDs.length < chunkIDs.length) {
             return false;
         }
-        for (int i = 0; i < chunks.length; i++) {
-            if (!chunks[i].equals(child.chunks[i])) {
+        for (int i = 0; i < chunkIDs.length; i++) {
+            if (chunkIDs[i] != child.chunkIDs[i]) {
                 return false;
             }
         }
         return true;
+    }
+
+    private static int[] packChunks(String[] chunks) {
+        int[] chunkIDs = new int[chunks.length];
+
+        for (int i = 0; i < chunks.length; i++) {
+            String chunk = chunks[i];
+
+            chunkIDs[i] = chunkIDMap.computeIfAbsent(chunk, k -> {
+                chunkValueMap.put(chunkIDMap.size(), k);
+                return chunkIDMap.size();
+            });
+        }
+
+        return chunkIDs;
+    }
+
+    private static String[] unpackChunks(int[] chunkIDs) {
+        String[] chunks = new String[chunkIDs.length];
+
+        for (int i = 0; i < chunks.length; i++) {
+            chunks[i] = chunkValueMap.get(i);
+        }
+
+        return chunks;
+    }
+
+    private String getChunk(int i) {
+        return chunkValueMap.get(i);
     }
 }
