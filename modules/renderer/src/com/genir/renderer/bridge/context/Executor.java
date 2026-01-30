33144a9a28a00181d3e46f5a1438a6213940d2fb
genir <45612481+Halke1986@users.noreply.github.com>
2026-01-30 02:04:39 +0100

    merge executors

--- modules/renderer/src/com/genir/renderer/bridge/context/Executor.java
+++ modules/renderer/src/com/genir/renderer/bridge/context/Executor.java
@@ -1,17 +1,43 @@
 package com.genir.renderer.bridge.context;
 
+import com.genir.renderer.async.ExecutorFactory;
 import com.genir.renderer.bridge.context.stall.StallDetector;
 import com.genir.renderer.debug.Profiler;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import java.util.Deque;
+import java.util.concurrent.*;
 
-public class Executor extends ExecutorCore {
+public class Executor {
     private final StallDetector stallDetector;
+
+    private Runnable[] currentFrame = new Runnable[1];
+    private int frameSize = 0;
+    private int maxFrameSize = 1;
+
+    private final ExecutorService execActual = ExecutorFactory.newSingleThreadExecutor("FR-Render");
+    private final Deque<Throwable> exceptions = new ConcurrentLinkedDeque<>();
+
+    private Future<?> lastFrameFuture = CompletableFuture.completedFuture(null);
 
     public Executor(StallDetector stallDetector) {
         this.stallDetector = stallDetector;
+    }
+
+    /**
+     * Queue command for execution.
+     */
+    public void execute(Runnable command) {
+        if (command == null) {
+            return;
+        }
+
+        if (currentFrame.length <= frameSize) {
+            maxFrameSize = currentFrame.length * 2;
+            currentFrame = BufferUtil.reallocate(Runnable.class, maxFrameSize, currentFrame);
+        }
+
+        currentFrame[frameSize] = command;
+        frameSize++;
     }
 
     /**
@@ -51,6 +77,25 @@ public class Executor extends ExecutorCore {
         }
     }
 
+    /**
+     * Execute queued commands.
+     */
+    public void swapFrames() {
+        rethrowLast();
+
+        final Runnable[] commands = currentFrame;
+
+        currentFrame = new Runnable[maxFrameSize];
+        frameSize = 0;
+
+        lastFrameFuture = execActual.submit(() -> executeCommands(commands));
+    }
+
+    /**
+     * Execute queued commands.
+     * Wait until PREVIOUS frame is completed. This allows producer
+     * and consumer threads to overlap with maximum flexibility.
+     */
     public void swapFramesAndSync() {
         Future<?> prevFrameFuture = lastFrameFuture;
 
@@ -64,6 +109,58 @@ public class Executor extends ExecutorCore {
             future.get();
         } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void executeCommands(Runnable[] commands) {
+        long start = System.nanoTime();
+
+        try {
+            // Run all scheduled commands.
+            for (int i = 0; i < commands.length && commands[i] != null; i++) {
+                Runnable command = commands[i];
+                commands[i] = null;
+
+                try {
+                    command.run();
+                } catch (Throwable t) {
+                    throw new RuntimeException(command.toString(), t);
+                }
+            }
+
+        } catch (Throwable t) {
+            exceptions.add(t);
+        } finally {
+            Profiler.profiler.frame.markRenderWork(start);
+        }
+    }
+
+    public void shutdown() {
+        execActual.shutdown();
+    }
+
+    /**
+     * Returns true if no commands are being executed.
+     */
+    public boolean isIdle() {
+        return lastFrameFuture.isDone();
+    }
+
+    private void rethrowLast() {
+        if (!exceptions.isEmpty()) {
+            Throwable t = exceptions.pop();
+            exceptions.clear();
+            rethrowAsRuntimeException(t);
+        }
+    }
+
+    private void rethrowAsRuntimeException(Throwable t) {
+        if (t == null) {
+            return;
+        } else if (t instanceof RuntimeException e) {
+            throw e;
+        } else {
+            throw new RuntimeException(t);
         }
     }
 }
