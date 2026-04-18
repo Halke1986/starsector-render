74e3bd904f691e2971859299b54ba16f1a18f3f2
genir <45612481+Halke1986@users.noreply.github.com>
2026-04-18 16:08:23 +0200

    optimized glBlendFuncSeparate

--- modules/renderer/src/com/genir/renderer/bridge/GL14.java
+++ modules/renderer/src/com/genir/renderer/bridge/GL14.java
@@ -19,15 +19,27 @@ public class GL14 {
         context.exec.execute(new glBlendEquation(mode));
     }
 
-    public static void glBlendFuncSeparate(int sfactorRGB, int dfactorRGB, int sfactorAlpha, int dfactorAlpha) {
-        record glBlendFuncSeparate(int sfactorRGB, int dfactorRGB, int sfactorAlpha, int dfactorAlpha) implements GLCommand, Recordable {
-            @Override
-            public void run(Context context, int[] args) {
-                context.attribManager.glBlendFuncSeparate(sfactorRGB, dfactorRGB, sfactorAlpha, dfactorAlpha);
-            }
-        }
+    static class GlBlendFuncSeparate implements GLCommand, Recordable {
+        @Override
+        public void run(Context context, int[] args) {
+            int sfactorRGB = args[0];
+            int dfactorRGB = args[1];
+            int sfactorAlpha = args[2];
+            int dfactorAlpha = args[3];
 
-        final Context context = getThreadContext();
-        context.exec.execute(new glBlendFuncSeparate(sfactorRGB, dfactorRGB, sfactorAlpha, dfactorAlpha));
+            context.attribManager.glBlendFuncSeparate(sfactorRGB, dfactorRGB, sfactorAlpha, dfactorAlpha);
+        }
+    }
+
+    static GlBlendFuncSeparate glBlendFuncSeparateCommand = new GlBlendFuncSeparate();
+
+    public static void glBlendFuncSeparate(int sfactorRGB, int dfactorRGB, int sfactorAlpha, int dfactorAlpha) {
+        getThreadContext().exec.execute(
+                glBlendFuncSeparateCommand,
+                sfactorRGB,
+                dfactorRGB,
+                sfactorAlpha,
+                dfactorAlpha
+        );
     }
 }
