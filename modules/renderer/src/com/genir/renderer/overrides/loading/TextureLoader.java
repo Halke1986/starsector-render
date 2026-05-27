6d2038550127324da08edf308b575ab8df12d130
genir <45612481+Halke1986@users.noreply.github.com>
2026-05-27 20:25:56 +0200

    override TextureLoader_loadTexture II

--- modules/renderer/src/com/genir/renderer/overrides/loading/TextureLoader.java
+++ modules/renderer/src/com/genir/renderer/overrides/loading/TextureLoader.java
@@ -77,6 +77,23 @@ public class TextureLoader {
         }
     }
 
+    public static TextureHandler loadTexture(Object delegate, TextureHandler target, String path, int var3, int var4, int var5, int var6, boolean generateSubImage) throws IOException {
+        // Delegate uncommon cases to vanilla.
+        if (target != null || var3 != GL11.GL_TEXTURE_2D || var4 != GL11.GL_RGBA || var5 != GL11.GL_LINEAR || var6 != GL11.GL_LINEAR || generateSubImage) {
+            return ((proxy.com.fs.graphics.TextureLoader) delegate).loadTexture_vanilla(target, path, var3, var4, var5, var6, generateSubImage);
+        }
+
+        logger.info("Loading image [" + path + "]");
+
+        BufferedImage image = proxy.com.fs.graphics.FileRepository.FileRepository_loadImage(path);
+        if (image == null) {
+            throw new RuntimeException("Image with filename [" + path + "] not found or failed to load");
+        }
+
+        TextureBuilder.TextureData texData = TextureBuilder.readAndAnalyzeImage(image);
+        return commitTexture(path, texData);
+    }
+
     /**
      * Commit texture to GPU and store a TextureHandler in TextureRepository.
      */
@@ -90,73 +107,37 @@ public class TextureLoader {
         }
     }
 
-    public static TextureHandler loadTexture(Object delegate, TextureHandler target, String path, int var3, int var4, int var5, int var6, boolean generateSubImage) throws IOException {
-        // Delegate uncommon cases to vanilla.
-//        if (target != null || var3 != GL11.GL_TEXTURE_2D || var4 != GL11.GL_RGBA || var5 != GL11.GL_LINEAR || var6 != GL11.GL_LINEAR || generateSubImage) {
-//            return delegate.loadTexture_vanilla(target, path, var3, var4, var5, var6, generateSubImage);
-//        }
+    /**
+     * Commit texture to GPU and return a TextureHandler.
+     */
+    private static TextureHandler commitTexture(String path, TextureBuilder.TextureData texData) {
+        final TextureHandler texture = new TextureHandler(GL11.GL_TEXTURE_2D, com.genir.renderer.bridge.GL11.glGenTextures(), path);
 
-        return ((proxy.com.fs.graphics.TextureLoader) delegate).loadTexture_vanilla(target, path, var3, var4, var5, var6, generateSubImage);
-//        // if target != null || generateSubImage -> call vanilla method_21178
-//
-//        // assert var3 == GL_TEXTURE_2D
-//        // assert var4 == GL_RGBA
-//        // assert var5 == GL_LINEAR
-//        // assert var6 == GL_LINEAR
-//
-//        short var8 = 0;
-//        if (target == null || target.getTextureID() == -1) {
-//            int var9 = this.method_21172();
-//            target = new TextureHandler(cGL_TEXTURE_2D, var9, path);
-//        }
-//
-//        target.setPath(path);
-//        target.method_25432();
-//        ByteBuffer var11 = null;
-//        if (var11 == null) {
-//            BufferedImage bufferedImage = this.method_21185(path);
-//            this.width = bufferedImage.getWidth();
-//            this.height = bufferedImage.getHeight();
-//            if (this.field_0 != null) {
-//                bufferedImage = this.field_0.method_567(bufferedImage);
-//            }
-//
-//            target.setImageWidth(bufferedImage.getWidth());
-//            target.setImageHeight(bufferedImage.getHeight());
-//            if (bufferedImage.getColorModel().hasAlpha()) {
-//                var8 = 6408;
-//            } else {
-//                var8 = 6407;
-//            }
-//
-//            var11 = this.analyzeTexture(bufferedImage, target);
-//            target.method_25448(this.color_1);
-//            target.method_25446(this.color_2);
-//            target.method_25450(this.color);
-//        }
-//
-//        boolean var12 = this.width <= 1024 && this.height <= 1024;
-//        if (set.contains(path)) {
-//            var12 = true;
-//        }
-//
-//        if (var12) {
-//            generateMipmap = false;
-//            GL11.glTexParameteri(cGL_TEXTURE_2D, 10241, 9987);
-//            GL11.glTexParameteri(cGL_TEXTURE_2D, 10240, var6);
-//            GL11.glTexParameteri(3553, 33169, 1);
-//        } else {
-//            GL11.glTexParameteri(cGL_TEXTURE_2D, 10241, var5);
-//            GL11.glTexParameteri(cGL_TEXTURE_2D, 10240, var6);
-//            GL11.glTexParameteri(cGL_TEXTURE_2D, 33169, 0);
-//        }
-//
-//        if (generateMipmap) {
-//            GL11.glTexSubImage2D(cGL_TEXTURE_2D, 0, 0, 0, this.method_21181(this.width), this.method_21181(this.height), var8, 5121, var11);
-//        } else {
-//            GL11.glTexImage2D(cGL_TEXTURE_2D, 0, var4, this.method_21181(this.width), this.method_21181(this.height), 0, var8, 5121, var11);
-//        }
-//
-//        return target;
+        texture.TextureHandler_setPath(path);
+        texture.TextureHandler_setImageWidth(texData.imageWidth);
+        texture.TextureHandler_setImageHeight(texData.imageHeight);
+        texture.TextureHandler_setHeight(texData.height);
+        texture.TextureHandler_setWidth(texData.width);
+        texture.TextureHandler_setColor0(texData.color0);
+        texture.TextureHandler_setColor1(texData.color1);
+        texture.TextureHandler_setColor2(texData.color2);
+
+        int colorType = texData.hasAlpha ? GL11.GL_RGBA : GL11.GL_RGB;
+        com.genir.renderer.bridge.GL11.glBindTexture(GL11.GL_TEXTURE_2D, texture.TextureHandler_getTextureID());
+
+        boolean generateMipmap = texData.width <= 1024 && texData.height <= 1024;
+        if (generateMipmap) {
+            com.genir.renderer.bridge.GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+            com.genir.renderer.bridge.GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+            com.genir.renderer.bridge.GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL14.GL_GENERATE_MIPMAP, 1);
+        } else {
+            com.genir.renderer.bridge.GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+            com.genir.renderer.bridge.GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+            com.genir.renderer.bridge.GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL14.GL_GENERATE_MIPMAP, 0);
+        }
+
+        com.genir.renderer.bridge.GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, texData.width, texData.height, 0, colorType, GL11.GL_UNSIGNED_BYTE, texData.buffer);
+
+        return texture;
     }
 }
