bc693d9c38bb94414611fb4aae70c426fc66537c
genir <45612481+Halke1986@users.noreply.github.com>
2026-08-15 00:47:35 +0200

    comment expected checksum

--- modules/agent/src/com/genir/renderer/agent/Agent.java
+++ modules/agent/src/com/genir/renderer/agent/Agent.java
@@ -12,6 +12,7 @@ import org.apache.log4j.Logger;
 
 public final class Agent {
     public static void premain(String agentArgs, Instrumentation instrumentation) {
+        // Expected Windows Starsector 0.98a-RC8 checksum: 5dd222b9e266d2ac2d63b3dad4983eb05caaf5a247d7dfb82aaeba47ea774cc8
         String checksum = getSha256(Path.of("starfarer_obf.jar"));
         Logger.getLogger(Agent.class).info("starfarer_obf.jar SHA-256 checksum: " + checksum);
 
