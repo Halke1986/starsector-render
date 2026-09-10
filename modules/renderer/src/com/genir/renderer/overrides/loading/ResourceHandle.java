dbb6637226b4b476a8929ba6cc7265731ca6a8a8
genir <45612481+Halke1986@users.noreply.github.com>
2026-09-10 18:07:27 +0200

    optimize imports

--- modules/renderer/src/com/genir/renderer/overrides/loading/ResourceHandle.java
+++ modules/renderer/src/com/genir/renderer/overrides/loading/ResourceHandle.java
@@ -1,11 +1,11 @@
 package com.genir.renderer.overrides.loading;
 
+import proxy.com.fs.util.FileLoader.ResourceLocation;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
-import proxy.com.fs.util.FileLoader.ResourceLocation;
 
 import static com.genir.renderer.overrides.loading.FileLoader.readStringVanilla;
 
